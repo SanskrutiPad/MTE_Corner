664a6e430d06ef6151f1e00e160ba15b7c70c452
SanskrutiPad <63366277+SanskrutiPad@users.noreply.github.com>
2022-04-26 09:26:06 -0700

    Initial Commit

---
+++ Maintenance Corner/Maintenance_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Maintenance_CornerApp.swift
+//  Maintenance Corner
+//
+//  Created by Sanskruti  Padmawar  on 2022-04-26.
+//
+
+import SwiftUI
+
+@main
+struct Maintenance_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
